3f0c7a194f61a2fe68cf92118754e3e4374d5bea
ksom10 <someson11@aol.com>
2021-03-11 19:35:25 -0500

    SalesPerson and Secretary
    added code to Salesperson and Secretary classes

--- sales_person.class.php
+++ sales_person.class.php
@@ -3,5 +3,39 @@
  * Author: Group 5
  * Date: 3/13/21
  * Name: sales_person.class.php
- * Description: 
+ * Description:
 */
+
+class SalesPerson extends Employee{
+
+    private $sales;
+    private $commission_rate;
+
+    public function __construct($person, $emp_id, $year_of_service, $sales, $commission_rate)
+    {
+        parent::__construct($person, $emp_id, $year_of_service);
+        $this->sales = $sales;
+        $this->commission_rate = $commission_rate;
+    }
+
+    public function getSales(){
+        return $this->sales;
+    }
+
+    public function getCommissionRate(){
+        return $this->commission_rate;
+    }
+
+    public function getPaymentAmount()
+    {
+        return $this->sales * $this->commission_rate;
+    }
+
+    public function toString()
+    {
+        parent::toString();
+        echo "<h3>Sales: $" . $this->getSales(). "</h3>";
+        echo "<h3>Commission Rate: " . $this->getCommissionRate() . "</h3>";
+        echo "<h3>Earning: $" . $this->getPaymentAmount() . "</h3>";
+    }
+}
--- secretary.class.php
+++ secretary.class.php
@@ -3,5 +3,42 @@
  * Author: Group 5
  * Date: 3/13/21
  * Name: secretary.class.php
- * Description: 
+ * Description:
 */
+
+class Secretary extends Employee{
+    private $wage;
+    private $hours;
+
+    public function __construct($person, $emp_id, $year_of_service, $wage, $hours)
+    {
+        parent::__construct($person, $emp_id, $year_of_service);
+        $this->wage = $wage;
+        $this->hours = $hours;
+    }
+
+    public function getWage(){
+        return $this->wage;
+    }
+
+    public function getHours(){
+        return $this->hours;
+    }
+
+    public function getPaymentAmount()
+    {
+        if ($this->hours > 40) {
+            return ($this->wage * $this->hours) + (($this->wage * $this->hours)* .150);
+        } else {
+            return $this->wage * $this->hours;
+        }
+    }
+
+    public function toString()
+    {
+        parent::toString();
+        echo "<h3>Wage: $" . $this->getWage() . "</h3>";
+        echo "<h3>Hours: " . $this->getHours() . "</h3>";
+        echo "<h3>Earning: $" . $this->getPaymentAmount() . "</h3>";
+    }
+}
