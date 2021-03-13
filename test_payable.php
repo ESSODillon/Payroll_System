68ad9c2fd21449fff258131b2fd07a589b2db074
bigGroupHub <80349370+bigGroupHub@users.noreply.github.com>
2021-03-13 11:16:44 -0500

    Update test_payable.php

--- test_payable.php
+++ test_payable.php
@@ -19,7 +19,7 @@ require "autoloading.php";
 
 <body>
 <?php
-
+/*
 
 echo "<h2>Payroll System Programmed with OOP</h2>";
 echo "<p>*****************************************************</p>";
@@ -30,9 +30,15 @@ echo "<div>", $iv1->toString(), "</div>";
 $iv2 = new Invoice("Stainless Steel Dog Bowl", 9.99, 2);
 echo "<div>", $iv2->toString(), "</div>";
 echo "<p>*****************************************************</p>";
+*/
 
+//Creating a Person tester
 echo "<h2>Testing the nameable interface with the Person class hierarchy</h2>";
 echo "<p>*****************************************************</p>";
+
+//Creating people
+//theses office people are then made in to employees
+$pe0 = new Person("Stanley", "Hudson", "Male", 286875138);
 
 $pe1 = new Person("James", "Smith", "Male", 327314323);
 echo "<div>", $pe1->toString(), "</div>";
@@ -64,36 +70,44 @@ echo "<div>", $em2->toString(), "</div>";
 echo "<p>*****************************************************</p>";
 */
 
-
+//Testing the Employee subclasses including Secretary
 echo "<h2>Testing the nameable interface with the Secretary class hierarchy</h2>";
 echo "<p>*****************************************************</p>";
 
+//pulling employee and echoing back to test
 $se1 = new Secretary($pe3, 38585578, 16, 22.02, 40);
 echo "<div>", $se1->toString(), "</div>";
 
 echo "<p>*****************************************************</p>";
 
+//Testing the Employee subclasses including SalesPerson
 echo "<h2>Testing the nameable interface with the SalesPerson class hierarchy</h2>";
 echo "<p>*****************************************************</p>";
 
+//pulling employee and echoing back to test
 $sa1 = new SalesPerson($pe4, 3584584, 19, 45, 46.20);
 echo "<div>", $sa1->toString(), "</div>";
 
+//Testing the Employee subclasses including Manager
 echo "<h2>Testing the nameable interface with the Manager class hierarchy</h2>";
 echo "<p>*****************************************************</p>";
 
+//pulling employee and echoing back to test
 $ma1 = new Manager($pe5, 38584583, 16,"sales", 80000);
 echo "<div>", $ma1->toString(), "</div>";
 
 echo "<p>*****************************************************</p>";
 
+//Testing the Employee subclasses including ExecutiveManger
 echo "<h2>Testing the nameable interface with the ExecutiveManager class hierarchy</h2>";
 echo "<p>*****************************************************</p>";
 
+//pulling employee and echoing back to test
 $ex1 = new ExecutiveManager($pe6, 1585489, 19, "sales", 96679, 2000);
 echo "<div>", $ex1->toString(), "</div>";
 
 echo "<p>*****************************************************</p>";
+
 
 ?>
 </body>
